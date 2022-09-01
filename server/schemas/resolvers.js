bb641294cf543284a70b20c77e4a139f686b11d1
B33Gs <speschk@yahoo.com>
2022-08-31 23:35:28 -0400

    Feature: User and Thought queries. -resolvers & typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,13 @@
+const { User, Thought } = require('../models')
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return 'Hello world!'
+    thoughts: async (username) => {
+      const params = username ? { username } : {}
+      return Thought.find(params).sort({ createdAt: -1 })
+    },
+    users: async () => {
+      return User.find()
     },
   },
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,26 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
+    users: [User]
   }
 `
 
